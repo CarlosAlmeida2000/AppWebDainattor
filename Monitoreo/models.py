f6a174d3fb6aad0595e66df1c03ff245f0075900
Carlosalmeida2000 <carlos.almeida2017@uteq.edu.ec>
2023-01-13 15:23:52 -0500

    Mejorando la predicción del trastorno

--- Monitoreo/models.py
+++ Monitoreo/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.db.models import Q, Value
-from django.db.models.functions import Concat
+from django.db.models import Q
 from Persona.models import Usuarios
-from Monitoreo.image import Image
-from sklearn import linear_model
 from random import sample
+import numpy as np
 
 # Create your models here.
 class Actividades(models.Model):
@@ -32,9 +30,7 @@ class Historial(models.Model):
 
                 total_dias = historial.order_by('-dia')[0]['dia']
                 # Si existe una semana de registro del historial, se predice el trastorno
-                prediccion = 'Para la predicción del trastorno, debe tener 7 días de registros en el historial.'
-                if(total_dias > 1):
-                    prediccion = Historial.prediccion_trastorno(total_dias, historial)
+                prediccion = Historial.prediccion_trastorno(total_dias, historial)
 
                 object_json =  { 
                 'dias_historial': total_dias,
@@ -67,18 +63,18 @@ class Historial(models.Model):
         frecuencia_triste = list()
         frecuencia_sorprendido = list()
         dias_historial = list()
-        predicciones_emocion = list()
+        predicciones_expresion = list()
 
-        dias_historial = [[(i + 1)] for i in range(total_dias)]
+        dias_historial = [(i + 1) for i in range(total_dias)]
 
         for d in dias_historial:
-            frecuencia_enfadado.append(historial.filter(Q(dia = d[0]) & Q(expresion_facial = 'Enfadado')).count())
-            frecuencia_disgustado.append(historial.filter(Q(dia = d[0]) & Q(expresion_facial = 'Disgustado')).count())
-            frecuencia_temeroso.append(historial.filter(Q(dia = d[0]) & Q(expresion_facial = 'Temeroso')).count())
-            frecuencia_feliz.append(historial.filter(Q(dia = d[0]) & Q(expresion_facial = 'Feliz')).count())
-            frecuencia_neutral.append(historial.filter(Q(dia = d[0]) & Q(expresion_facial = 'Neutral')).count())
-            frecuencia_triste.append(historial.filter(Q(dia = d[0]) & Q(expresion_facial = 'Triste')).count())
-            frecuencia_sorprendido.append(historial.filter(Q(dia = d[0]) & Q(expresion_facial = 'Sorprendido')).count())
+            frecuencia_enfadado.append(historial.filter(Q(dia = d) & Q(expresion_facial = 'Enfadado')).count())
+            frecuencia_disgustado.append(historial.filter(Q(dia = d) & Q(expresion_facial = 'Disgustado')).count())
+            frecuencia_temeroso.append(historial.filter(Q(dia = d) & Q(expresion_facial = 'Temeroso')).count())
+            frecuencia_feliz.append(historial.filter(Q(dia = d) & Q(expresion_facial = 'Feliz')).count())
+            frecuencia_neutral.append(historial.filter(Q(dia = d) & Q(expresion_facial = 'Neutral')).count())
+            frecuencia_triste.append(historial.filter(Q(dia = d) & Q(expresion_facial = 'Triste')).count())
+            frecuencia_sorprendido.append(historial.filter(Q(dia = d) & Q(expresion_facial = 'Sorprendido')).count())
         
         print('Enfadado: ', frecuencia_enfadado)
         print('Disgustado: ', frecuencia_disgustado)
@@ -88,36 +84,35 @@ class Historial(models.Model):
         print('Triste:', frecuencia_triste)
         print('Sorprendido: ', frecuencia_sorprendido)
 
-        predicciones_emocion.append(Historial.regresion_logistica(dias_historial, frecuencia_enfadado, (total_dias + 1)))
-        predicciones_emocion.append(Historial.regresion_logistica(dias_historial, frecuencia_disgustado, (total_dias + 1)))
-        predicciones_emocion.append(Historial.regresion_logistica(dias_historial, frecuencia_temeroso, (total_dias + 1)))
-        predicciones_emocion.append(Historial.regresion_logistica(dias_historial, frecuencia_feliz, (total_dias + 1)))
-        predicciones_emocion.append(Historial.regresion_logistica(dias_historial, frecuencia_neutral, (total_dias + 1)))
-        predicciones_emocion.append(Historial.regresion_logistica(dias_historial, frecuencia_triste, (total_dias + 1)))
-        predicciones_emocion.append(Historial.regresion_logistica(dias_historial, frecuencia_sorprendido, (total_dias + 1)))
+        predicciones_expresion.append(Historial.regresion_polinomial(dias_historial, frecuencia_enfadado, (total_dias + 1)))
+        predicciones_expresion.append(Historial.regresion_polinomial(dias_historial, frecuencia_disgustado, (total_dias + 1)))
+        predicciones_expresion.append(Historial.regresion_polinomial(dias_historial, frecuencia_temeroso, (total_dias + 1)))
+        predicciones_expresion.append(Historial.regresion_polinomial(dias_historial, frecuencia_feliz, (total_dias + 1)))
+        predicciones_expresion.append(Historial.regresion_polinomial(dias_historial, frecuencia_neutral, (total_dias + 1)))
+        predicciones_expresion.append(Historial.regresion_polinomial(dias_historial, frecuencia_triste, (total_dias + 1)))
+        predicciones_expresion.append(Historial.regresion_polinomial(dias_historial, frecuencia_sorprendido, (total_dias + 1)))
 
-        print('prediciones de cada emoción: ', predicciones_emocion, '\n') 
+        print('prediciones de cada expresión: ', predicciones_expresion, '\n') 
 
-        maxima_prediccion = max(predicciones_emocion)
-        emocion_predecida = emotion_dict[predicciones_emocion.index(maxima_prediccion)]
+        maxima_prediccion = max(predicciones_expresion)
+        expresion_predecida = emotion_dict[predicciones_expresion.index(maxima_prediccion)]
         
-        print(emocion_predecida, maxima_prediccion)
-        if emocion_predecida == 'Enfadado':
-            return 'Trastorno explosivo intermitente con una predicción de aparición futura de la emoción de enfadado de {0} veces.'.format(maxima_prediccion)
-        elif emocion_predecida == 'Disgustado':
-            return 'Trastorno obsesivo-compulsivo con una predicción de aparición futura de la emoción de disgustado de {0} veces'.format(maxima_prediccion)
-        elif emocion_predecida == 'Temeroso':
-            return 'Trastorno de la personalidad con predicción de aparición futura de la emoción temeroso de {0} veces'.format(maxima_prediccion)
-        elif emocion_predecida == 'Triste':
-            return 'Trastorno depresivo con una predicción de aparición futura de la emoción triste de {0} veces'.format(maxima_prediccion)
-        elif emocion_predecida == 'Feliz' or emocion_predecida == 'Neutral' or emocion_predecida == 'Sorprendido':
-            return 'Ningún trastorno'
+        if expresion_predecida == 'Enfadado':
+            return 'Se predice un posible Trastorno explosivo intermitente, se estima que a futuro se presente {0} veces la expresión de enfadado.'.format(int(round(maxima_prediccion, 2)))
+        elif expresion_predecida == 'Disgustado':
+            return 'Se predice un posible Trastorno obsesivo-compulsivo, se estima que a futuro se presente {0} veces la expresión de disgustado'.format(int(round(maxima_prediccion, 2)))
+        elif expresion_predecida == 'Temeroso':
+            return 'Se predice un posible Trastorno de la personalidad, se estima que a futuro se presente {0} veces la expresión de temeroso.'.format(int(round(maxima_prediccion, 2)))
+        elif expresion_predecida == 'Triste':
+            return 'Se predice un posible Trastorno depresivo, se estima que a futuro se presente {0} veces la expresión de triste.'.format(int(round(maxima_prediccion, 2)))
+        elif expresion_predecida == 'Feliz' or expresion_predecida == 'Neutral' or expresion_predecida == 'Sorprendido':
+            return 'No presenta ningún trastorno emocional.'
 
     @staticmethod
-    def regresion_logistica(x_train, y_train, x_prediction):
-        # Creamos el objeto de Regresión Logística
-        regresion = linear_model.LogisticRegression()
-        # Entrenamos nuestro modelo
-        regresion.fit(x_train, y_train) 
-        # Predicción de ocurrencua de una emoción dado un día x
-        return int(regresion.predict([[x_prediction]]))
+    def regresion_polinomial(x_train, y_train, x_prediction):
+        # polinomio de grado 3
+        modelo = np.poly1d(np.polyfit(x_train, y_train, 3))
+        resultado_y = 0
+        print("Ecuación polinómica: \n", modelo)
+        resultado_y = ((modelo[3]) * (x_prediction) ** 3) + ((modelo[2]) * (x_prediction) ** 2) + ((modelo[1]) * (x_prediction) ** 1) + modelo[1]
+        return resultado_y
